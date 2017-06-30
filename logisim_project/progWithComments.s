80b73e3e405df4c6bd1d3af920a0c07ff941d833
piernov <piernov@piernov.org>
2017-06-30 14:52:47 +0200

    logisim_project cleanups + add demo program

--- logisim_project/progWithComments.s
+++
@@ -1,8 +0,0 @@
-LDR R0, #0 //Chargement de la valeur de la case 0 de la RAM dans le registre 0
-LDR R1, #0 //Chargement de la valeur de la case 0 de la RAM dans le registre 1
-SUB R0, R1, R0 // R0 prend la valeur de R1-R0
-BRANCH 5 //Si R0==0 goto instruction de la ram à l'adresse 5 (la 6e) sinon ignoré
-LDR R2, #2 //Ignoré
-LDR R0, #1 //Chargement de la valeur de la case 1 de la RAM dans le registre 0
-LSL R1, R1, #3 //Décalage de 3 à gauche de la valeur de R1 dans R1
-ORR R0, R1 //OU logique R1 et R0 dans R0
