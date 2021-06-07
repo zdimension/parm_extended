08d255cb931f199ebba0172fb61ae1626da755ee
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-06-07 12:28:22 +0200

    New string format can read and write

--- code_c/string2.c
+++ code_c/string2.c
@@ -1,13 +1,41 @@
 #include <parm.h>
+#include <stdio.h>
+
+//asm("adds r6, %[v], #0" : : [v] "r" (str[0]) : "r6");
 
 void run()
 {
     BEGIN();
-    unsigned int t='a';
-    volatile unsigned int str[5];
+    //unsigned int t='a';
+    unsigned int target='b';
+    unsigned int result;
+    /*volatile*/ unsigned int str[5];
+
+    // WRITE
     int i=2;
     str[0]=0;
     str[0]='a' | i << 16;
+    str[0] |= target << 24;
+    while(((str[0] >> 16) & 127) != ((str[0] >> 8) & 127))
+    {
+        str[1] = str[1] & 127;
+        str[1] |= ((str[0] >> 24) & 127) << 24;
+        str[1] |= ((str[0] >> 16) & 127) << 16;
+        str[1] |= (((str[0] >> 8) + 1 ) & 127) << 8;
+        asm("add sp, #4");
+    }
+    str[0] |= str[0] >> 24;
+    while((str[0] >> 8) & 127)
+    {
+        asm("sub sp, #4");
+    }
+    RES=i; // Check SP is ok
+
+    // WRITE
+
+    str[0] &= 127;
+    str[0] |= i<<16;
+
     while(((str[0] >> 16) & 127) != ((str[0] >> 8) & 127))
     {
         str[1] = str[1] & 127;
@@ -15,11 +43,20 @@ void run()
         str[1] |= (((str[0] >> 8) + 1 ) & 127) << 8;
         asm("add sp, #4");
     }
-    str[0]|='b';
+    str[0] |= str[0] << 24;
     while((str[0] >> 8) & 127)
     {
         asm("sub sp, #4");
+        str[0] |= ((str[1] >> 24) & 127) << 24;
     }
-    RES=i;
+    RES=i+1;
+    /*str[0] = str[0] >> 8;
+    str[0] = str[0] << 8;*/
+    result=0;
+    result=str[0];
+    result = result >> 12;
+    result = result >> 12;
+    //str[0] &= 127;
+    PUTCHAR(result);
     END();
 }
--- code_c/string2.s
+++ code_c/string2.s
@@ -25,16 +25,16 @@
 	.thumb_func
 run:
 	.fnstart
-	.pad	#112
-	sub	sp, #112
+	.pad	#116
+	sub	sp, #116
 	@APP
 	sub	sp, #508
 	@NO_APP
 	@APP
 	sub	sp, #452
 	@NO_APP
-	movs	r0, #97
-	str	r0, [sp, #24]
+	movs	r0, #98
+	str	r0, [sp, #28]
 	movs	r0, #2
 	str	r0, [sp]
 	movs	r0, #0
@@ -43,18 +43,91 @@ run:
 	lsls	r0, r0, #16
 	adds	r0, #97
 	str	r0, [sp, #4]
+	ldr	r0, [sp, #28]
+	lsls	r0, r0, #24
+	ldr	r1, [sp, #4]
+	orrs	r1, r0
+	str	r1, [sp, #4]
 	b	.LBB0_1
 .LBB0_1:
 	ldr	r0, [sp, #4]
-	lsls	r0, r0, #9
-	lsrs	r0, r0, #25
-	ldr	r1, [sp, #4]
-	lsls	r1, r1, #17
+	lsls	r1, r0, #9
 	lsrs	r1, r1, #25
-	cmp	r0, r1
+	lsls	r0, r0, #17
+	lsrs	r0, r0, #25
+	cmp	r1, r0
 	beq	.LBB0_3
 	b	.LBB0_2
 .LBB0_2:
+	ldr	r0, [sp, #8]
+	movs	r1, #127
+	ands	r0, r1
+	str	r0, [sp, #8]
+	ldr	r0, [sp, #4]
+	lsls	r2, r1, #24
+	ands	r0, r2
+	ldr	r2, [sp, #8]
+	orrs	r2, r0
+	str	r2, [sp, #8]
+	ldr	r0, [sp, #4]
+	lsls	r2, r1, #16
+	ands	r0, r2
+	ldr	r2, [sp, #8]
+	orrs	r2, r0
+	str	r2, [sp, #8]
+	ldr	r0, [sp, #4]
+	lsrs	r0, r0, #8
+	adds	r0, r0, #1
+	ands	r0, r1
+	lsls	r0, r0, #8
+	ldr	r1, [sp, #8]
+	orrs	r1, r0
+	str	r1, [sp, #8]
+	@APP
+	add	sp, #4
+	@NO_APP
+	b	.LBB0_1
+.LBB0_3:
+	ldr	r0, [sp, #4]
+	lsrs	r1, r0, #24
+	orrs	r0, r1
+	str	r0, [sp, #4]
+	b	.LBB0_4
+.LBB0_4:
+	ldr	r0, [sp, #4]
+	lsls	r0, r0, #17
+	lsrs	r0, r0, #25
+	cmp	r0, #0
+	beq	.LBB0_6
+	b	.LBB0_5
+.LBB0_5:
+	@APP
+	sub	sp, #4
+	@NO_APP
+	b	.LBB0_4
+.LBB0_6:
+	ldr	r0, [sp]
+	str	r0, [sp, #56]
+	ldr	r0, [sp, #4]
+	movs	r1, #127
+	ands	r0, r1
+	str	r0, [sp, #4]
+	ldr	r0, [sp]
+	lsls	r0, r0, #16
+	ldr	r1, [sp, #4]
+	orrs	r1, r0
+	str	r1, [sp, #4]
+	b	.LBB0_7
+.LBB0_7:
+	ldr	r0, [sp, #4]
+	lsls	r1, r0, #9
+	lsrs	r1, r1, #25
+	lsls	r0, r0, #17
+	lsrs	r0, r0, #25
+	cmp	r1, r0
+	beq	.LBB0_9
+	b	.LBB0_8
+.LBB0_8:
 	ldr	r0, [sp, #8]
 	movs	r1, #127
 	ands	r0, r1
@@ -76,33 +149,57 @@ run:
 	@APP
 	add	sp, #4
 	@NO_APP
-	b	.LBB0_1
-.LBB0_3:
+	b	.LBB0_7
+.LBB0_9:
 	ldr	r0, [sp, #4]
-	movs	r1, #98
+	lsls	r1, r0, #24
 	orrs	r0, r1
 	str	r0, [sp, #4]
-	b	.LBB0_4
-.LBB0_4:
+	b	.LBB0_10
+.LBB0_10:
 	ldr	r0, [sp, #4]
 	lsls	r0, r0, #17
 	lsrs	r0, r0, #25
 	cmp	r0, #0
-	beq	.LBB0_6
-	b	.LBB0_5
-.LBB0_5:
+	beq	.LBB0_12
+	b	.LBB0_11
+.LBB0_11:
 	@APP
 	sub	sp, #4
 	@NO_APP
-	b	.LBB0_4
-.LBB0_6:
+	ldr	r0, [sp, #8]
+	movs	r1, #127
+	lsls	r1, r1, #24
+	ands	r0, r1
+	ldr	r1, [sp, #4]
+	orrs	r1, r0
+	str	r1, [sp, #4]
+	b	.LBB0_10
+.LBB0_12:
 	ldr	r0, [sp]
+	adds	r0, r0, #1
+	str	r0, [sp, #56]
+	movs	r0, #0
+	str	r0, [sp, #24]
+	ldr	r0, [sp, #4]
+	str	r0, [sp, #24]
+	ldr	r0, [sp, #24]
+	lsrs	r0, r0, #12
+	str	r0, [sp, #24]
+	ldr	r0, [sp, #24]
+	lsrs	r0, r0, #12
+	str	r0, [sp, #24]
+	b	.LBB0_13
+.LBB0_13:
+	ldr	r0, [sp, #24]
 	str	r0, [sp, #52]
-	b	.LBB0_7
-.LBB0_7:
-	b	.LBB0_8
-.LBB0_8:
-	b	.LBB0_8
+	b	.LBB0_14
+.LBB0_14:
+	b	.LBB0_15
+.LBB0_15:
+	b	.LBB0_16
+.LBB0_16:
+	b	.LBB0_16
 .Lfunc_end0:
 	.size	run, .Lfunc_end0-run
 	.cantunwind
