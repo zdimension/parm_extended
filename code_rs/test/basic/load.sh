df537b25e20669559f07b24bd566a35ba1b8ec6e
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-08-02 08:02:10 +0200

    exit nc after send

--- code_rs/test/basic/load.sh
+++ code_rs/test/basic/load.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat $1 <(echo) | dos2unix | iconv -f utf8 -t ascii//TRANSLIT | nc ::1 4567
+cat $1 <(echo) | dos2unix | iconv -f utf8 -t ascii//TRANSLIT | nc -N ::1 4567
--- code_rs/test/scheme/load.sh
+++ code_rs/test/scheme/load.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat $1 <(echo) | sed 's/\xce\xbb/lambda/g' | dos2unix | iconv -f utf8 -t ascii//TRANSLIT | nc ::1 4567
+cat $1 <(echo) | sed 's/\xce\xbb/lambda/g' | dos2unix | iconv -f utf8 -t ascii//TRANSLIT | nc -N ::1 4567
