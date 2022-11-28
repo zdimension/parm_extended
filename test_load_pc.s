e5f045ab95ea6b4224c65dfc3da7a6fb1bbe0119
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-11-28 14:05:22 +0100

    push and pop work (except if next to each other)

--- test_load_pc.s
+++ test_load_pc.s
@@ -4,8 +4,20 @@ mov sp, r0
 movs r0, #0
 
 
-movs r2, #1
+movs r2, #2
+movs r3, #3
+bl func 
+movs r4, #4
+movs r5, #5
+
+func:
 push {r2, lr}
+movs r2, #5
+pop {r2, pc}
+
+
+
+
 movs r2, #2
 str r1, [sp]
 movs r4, #4
